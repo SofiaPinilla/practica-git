b5468daaeffd5273b9d864ff55686b0835bdf5ae
SofiaPinilla <contact@sofiapinilla.com>
2022-11-15 09:48:39 +0100

    endpoint categorias creado

--- index.js
+++ index.js
@@ -3,4 +3,7 @@ const app = express()
 require("dotenv").config()
 const PORT = process.env.PORT || 3001
 
+app.get("/categories",(req,res)=>{
+    res.send("toda las categorias")
+})
 app.listen(PORT,()=>console.log(`Server started on port ${PORT}`))
